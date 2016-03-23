1e8f6a9b231653463ff3d89dfc1420818135848c
tcooper <tcooper@localhost>
2016-03-23 17:33:07 -0400

    initial commit

---
+++ SimpleBattleship/src/edu/saintjoe/cs/tcooper/SimpleBattleship/SimpleDotComTest.java
@@ -0,0 +1,21 @@
+package edu.saintjoe.cs.tcooper.SimpleBattleship;
+
+public class SimpleDotComTest {
+
+	public static void main(String[] args) {
+		SimpleDotCom dot = new SimpleDotCom();
+		
+		int[] locations = {2,3,4,};
+		dot.setLocationCells(locations);
+		
+		String userGuess = "2";
+		String result = dot.checkYourself(userGuess);
+		String testResult = "failed";
+		if (result.equals("hit")) {
+			testResult = "passed";
+		}
+		System.out.println(testResult);
+	}
+
+}
+
