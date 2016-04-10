e0b07591712dde0066a708adef23bd729ebbcedb
tcooper <tcooper@localhost>
2016-04-10 18:48:00 -0400

    Added GameHelper class

--- SimpleBattleship/src/edu/saintjoe/cs/tcooper/SimpleBattleship/Game.java
+++ SimpleBattleship/src/edu/saintjoe/cs/tcooper/SimpleBattleship/Game.java
@@ -10,7 +10,7 @@ public class Game {
 		int randomNum = (int) (Math.random() * 5);
 		
 		int[] locations = {randomNum, randomNum+1, randomNum+2};
-		theDotCom.setLocationcells(locations);
+		theDotCom.setLocationCells(locations);
 		boolean isAlive = true;
 		
 		while(isAlive == true) {
